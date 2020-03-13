70c6636317bcd41d21dc22ace9486e4560b85cc0
User <uzairbangee@gmail.com>
2020-03-13 21:22:53 +0500

    Updates

---
+++ precache-manifest.12c7e979178a170e27ff185b31f0f4eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eee948059abba1b14d5ffb4c0f9df01",
+    "url": "/QuizApp/index.html"
+  },
+  {
+    "revision": "e2f0c0e315cfb0c73426",
+    "url": "/QuizApp/static/css/main.bf6e3bc5.chunk.css"
+  },
+  {
+    "revision": "5eaa4f8bf960237f70b8",
+    "url": "/QuizApp/static/js/2.8dae67a8.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/QuizApp/static/js/2.8dae67a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2f0c0e315cfb0c73426",
+    "url": "/QuizApp/static/js/main.4ffd59c3.chunk.js"
+  },
+  {
+    "revision": "0368930f1f59622d0453",
+    "url": "/QuizApp/static/js/runtime-main.93b579c2.js"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/QuizApp/static/media/logo.47ce6e77.png"
+  }
+]);
